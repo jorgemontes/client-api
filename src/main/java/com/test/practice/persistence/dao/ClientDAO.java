2b22456594bfdeb73c9788dc547ea6e1a10b1d8c
jorgemontes <jamaff86@gmail.com>
2017-05-04 21:54:57 -0500

    initial commmit

---
+++ src/main/java/com/test/practice/persistence/dao/ClientDAO.java
@@ -0,0 +1,33 @@
+package com.test.practice.persistence.dao;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.test.practice.persistence.entity.Client;
+
+@Stateless
+public class ClientDAO {
+
+	@PersistenceContext(unitName = "clientPersistenceUnit")
+	private EntityManager entityManager;
+
+	public void saveClient(Client client) {
+		this.entityManager.persist(client);
+	}
+
+	public void updateClient(Client client) {
+		this.entityManager.merge(client);
+	}
+
+	public Client getClient(String id) {
+		return this.entityManager.find(Client.class, id);
+	}
+
+	public List<Client> getAllClients() {
+		return this.entityManager.createQuery("from " + Client.class.getSimpleName()).getResultList();
+	}
+
+}
---
+++ src/main/java/com/test/practice/service/rest/ClientService.java
@@ -0,0 +1,37 @@
+package com.test.practice.service.rest;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.test.practice.persistence.dao.ClientDAO;
+import com.test.practice.persistence.entity.Client;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Path("/client")
+@Api(value = "/client")
+@Stateless
+public class ClientService {
+
+	@EJB
+	private ClientDAO clientDAO;
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "Get All clients")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "OK"), @ApiResponse(code = 500, message = "UhOh") })
+
+	public List<Client> getAllClients() {
+		return this.clientDAO.getAllClients();
+	}
+
+}
